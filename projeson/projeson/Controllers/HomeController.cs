1c81195e4b760a8e788485e4d5a675c1e813f7a5
Hüseyin Karasu <krsshuseyinn@gmail.com>
2023-02-25 04:14:53 +0300

    umul yazılım case
    umul yazılım case

---
+++ projeson/projeson/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using projeson.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projeson.Controllers
+{
+    public class HomeController : Controller
+    {
+        tasımaEntities3 db = new tasımaEntities3();
+        tasımatablosu t = new tasımatablosu();
+        [HttpGet]
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(tasımatablosu t)
+        {
+           
+            db.tasımatablosu.Add(t);
+            db.SaveChanges();
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+          
+
+            var tasıma = db.tasımatablosu.ToList();
+            return View(tasıma);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult sil(int id)
+        {
+            var ts = db.tasımatablosu.Find(id);
+            db.tasımatablosu.Remove(ts);
+            db.SaveChanges();
+            return RedirectToAction("About");
+        }
+    }
+}
---
+++ projeson/projeson/Controllers/tasımaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projeson.Controllers
+{
+    public class tasımaController : Controller
+    {
+        // GET: tasıma
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
